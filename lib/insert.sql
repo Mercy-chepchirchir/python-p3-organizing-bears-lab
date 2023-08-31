ae82a071340cc18abe7fc6be640707b1723383d0
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-08-31 15:02:49 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears (id ,name, age, sex , color , temperament , alive) 
+VALUES 
+       (1, 'Mr. Chocolate', 20, 'M','brown','energetic', 0),
+       (2, 'Rowdy', 19, 'M','black','artistic', 1),
+       (3, 'Tabitha', 16, 'F','brown','outgoing', 0),
+       (4, 'Sergeant Brown', 22, 'M','black','choleric', 1),
+       (5, 'Melissa', 18, 'F','brown','optimistic', 1),
+       (6, 'Grinch', 20, 'M','black','sanguine', 0),
+       (7, 'Wendy', 21, 'F','brown','extroverted', 0),
+       (8, NULL, 23, 'F','black','extroverted', 1)
+;      
+
